c90a7ddc7c737b3004f90d971db36552ada9158c
Mime Jr <petravidovic8@gmail.com>
2023-01-18 23:23:16 -0500

    add styling to navigation

--- my-app/src/components/Navigation/Navigation.js
+++ my-app/src/components/Navigation/Navigation.js
@@ -6,10 +6,10 @@ export default function Navigation(props) {
     <>
     <nav>
     <img src='../cloudbackground.png' alt='purple clouds'/>
-    <h2 onClick={() => props.changePage("aboutme")}>About me</h2>
-    <h2 onClick={() => props.changePage("project")}>Projects</h2>
-    <h2 onClick={() => props.changePage("contact")}>Contact</h2>
-    <h2 onClick={() => props.changePage("resume")}>Resume</h2>
+    <h2 class="navh2" onClick={() => props.changePage("aboutme")}>About me</h2>
+    <h2 class="navh2" onClick={() => props.changePage("project")}>Projects</h2>
+    <h2 class="navh2" onClick={() => props.changePage("contact")}>Contact</h2>
+    <h2 class="navh2" onClick={() => props.changePage("resume")}>Resume</h2>
     </nav>
     </>
   )
