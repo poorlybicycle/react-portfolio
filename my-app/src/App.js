f225a4ec685057482e318b10f83a8e3dbf183671
Mime Jr <petravidovic8@gmail.com>
2023-01-18 23:02:33 -0500

    import files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Project from './components/Pages/Project/Project';
+import Aboutme from './components/Pages/Aboutme/Aboutme';
+import Contact from './components/Pages/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
